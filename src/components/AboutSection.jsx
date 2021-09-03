ad078271d574e25edb8427600bd828637164cbc3
bradleyshao <SYT1108@OUTLOOK.COM>
2021-09-03 05:56:38 -0700

    step 2 & 3

---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import home1 from '../img/IMG_0001_Original.jpg'
+const AboutSection =()=>{
+    return(
+        <div>
+            <div className="description">
+                <div className="title">
+                    <div className="hide">
+                        <h2>We work to make</h2>
+                    </div>
+                    <div className="hide">
+                        <h2>your <span>dreams come</span></h2>
+                    </div>
+                    <div className="hide">
+                        <h2>true.</h2>
+                    </div>
+                </div>
+                <p>
+                    Contact us for any photograhy ideas that you have. We have professionals with amazing skills.
+                </p>
+                <button>Contact Us</button>
+            </div>
+            <div className="image">
+                <img src={home1} alt="bear picture" />
+            </div>
+        </div>
+    );
+}
+
+
+export default AboutSection;
