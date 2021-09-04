ee83c7987dae1a10f2bb92613ec89e662e83faf1
bradleyshao <SYT1108@OUTLOOK.COM>
2021-09-04 05:08:08 -0700

    Styled AboutSection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import AboutPage from './pages/AboutPage'
+import GlobalStyle from './components/GlobalStyle';
 function App() {
   return (
     <div className="App">
       <h1>Film</h1>
+      <GlobalStyle/>
       <AboutPage/>
     </div>
   );
