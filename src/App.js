a1e4658b506f6a1355501129fc93e4c4907a2a04
bradleyshao <SYT1108@OUTLOOK.COM>
2021-09-07 02:19:34 -0700

    Nav
    Add Nav bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AboutPage from './pages/AboutPage'
 import GlobalStyle from './components/GlobalStyle';
+import Nav from "./components/Nav"
 function App() {
   return (
     <div className="App">
       
       <GlobalStyle/>
+      <Nav/>
       <AboutPage/>
     </div>
   );
